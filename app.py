a12e8b4071ed4d806da070b1d2fa8bac0ef7a4fa
Rafael Takeguma <rafael08.0807@gmail.com>
2024-03-06 20:24:11 -0300

    corrige user, password e dbname

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import psycopg2
 
 host = 'database-1.clidhukfcnn3.us-east-1.rds.amazonaws.com'
 
-bd_nome = 'database-1'
+bd_nome = 'postgres'
 
-usuario_01 = senha_01 = 'aluno'
+usuario_01 = "rafaelseto"
+
+senha_01 = 'professor'
 
 conexao = psycopg2.connect(host=host, dbname=bd_nome, user=usuario_01, password=senha_01)
 print('Criou a conexao')
@@ -13,13 +15,13 @@ print('Criou a conexao')
 cursor = conexao.cursor()
 
 # Comando de teste SQL
-SQL_select_users = ""
+SQL_select_endereco = "SELECT * FROM mydb.endereco"
 
 # Executando o comando de teste
-cursor.execute(SQL_select_users)
+cursor.execute(SQL_select_endereco)
 
 # Imprime a consulta SQL
-print(SQL_select_users)
+print(SQL_select_endereco)
 
 # Imprime registros retornados pela consulta SQL
 for record in cursor:
